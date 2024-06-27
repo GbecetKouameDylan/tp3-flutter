c6a1d58b93a85208a46cca49cc5b9827ede508ae
Gbecet Kouame Djahakpl <2164425@cegepmontpetit.ca>
2024-06-27 11:47:44 -0400

    FCT"Ajout d'un signout

--- tp3_flutter/lib/main.dart
+++ tp3_flutter/lib/main.dart
@@ -109,21 +109,25 @@ class _MyHomePageState extends State<MyHomePage> {
 
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            ElevatedButton(
+              onPressed: () async {
+                await GoogleSignIn().signOut();
+                await FirebaseAuth.instance.signOut();
+                setState(() {});
+              },
+              child: Text("signout"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () async {
+               signInWithGoogle();
+              },
+              child: Text("signin"),
             ),
+
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: signInWithGoogle,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+
     );
   }
 }
